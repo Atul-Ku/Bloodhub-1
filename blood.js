e9ac50e8ce76042d14dac9b523a43b60c851790d
Aman Raj <97393917+Amanraj40@users.noreply.github.com>
2023-06-30 20:41:36 +0530

    first commit

---
+++ blood.js
@@ -0,0 +1,24 @@
+// Get the elements
+var decrementButton = document.getElementById("decrement");
+var incrementButton = document.getElementById("increment");
+var countSpan = document.getElementById("count");
+
+// Initial count value
+var count = 0;
+
+// Function to update the count span
+function updateCount() {
+  countSpan.textContent = count;
+}
+
+// Event listener for the decrement button
+decrementButton.addEventListener("click", function() {
+  count--;
+  updateCount();
+});
+
+// Event listener for the increment button
+incrementButton.addEventListener("click", function() {
+  count++;
+  updateCount();
+});
